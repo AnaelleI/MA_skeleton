3c355525d206b4546637095eda769c1a8327faa8
alexgus <alex.guyon78@gmail.com>
2016-05-28 15:00:18 +0200

    Basic web views and controllers with some routes

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -8,12 +8,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
-
-    public function showAction(Request $request)
+    public function homeAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render(
+                'default/index.html.twig', 
+                [
+                    'title' => "Home",
+                    'content' => "Nothing to display for now"
+                ]
+            );
+    }
+
+    public function eventAction(Request $request)
+    {
+        return $this->render(
+                'default/index.html.twig', 
+                [
+                    'title' => "Event",
+                    'content' => "Coucou ça gaz ?"
+                ]
+            );
     }
 }
