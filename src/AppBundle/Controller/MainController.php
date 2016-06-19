ab8f01f404057ffadedc44f22464ba33ca244675
alexgus <alex.guyon78@gmail.com>
2016-06-19 15:38:02 +0200

    People routing

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -11,7 +11,7 @@ class MainController extends Controller
     public function homeAction(Request $request)
     {
         return $this->render(
-                'default/index.html.twig', 
+                'default/home.html.twig', 
                 [
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
                     'title' => "Home",
@@ -23,11 +23,37 @@ class MainController extends Controller
     public function eventAction(Request $request)
     {
         return $this->render(
-                'default/index.html.twig', 
+                'default/home.html.twig', 
                 [
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
                     'title' => "Event",
                     'content' => "Coucou ça gaz ?"
+                ]
+            );
+    }
+
+    public function noPeopleAction(Request $request)
+    {
+        return $this->render(
+                'default/error.html.twig', 
+                [
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+                    'title' => "Error",
+                    'error' => "No user selected"
+                ]
+            );
+    }
+
+    public function peopleAction(Request $request, $people)
+    {
+        // TODO FIND people
+
+        return $this->render(
+                'default/people.html.twig', 
+                [
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+                    'title' => "People:".$people,
+                    'content' => "Coucou ça gaz ".$people." ?"
                 ]
             );
     }
